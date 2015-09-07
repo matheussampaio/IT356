6d07ee5cfb8b2c06cf99614fd959cd23c04857cf
Matheus Sampaio <matheus1401@gmail.com>
2015-09-07 17:15:48 -0500

    Tabs converted to whitespaces

--- Test/Test/Maze.cpp
+++ Test/Test/Maze.cpp
@@ -11,30 +11,30 @@ class Maze : public sf::Drawable, public sf::Transformable
 {
 public:
 
-	bool init(string filename, int screenWidth, int screenHeigth)
-	{
-		load(filename);
+    bool init(string filename, int screenWidth, int screenHeigth)
+    {
+        load(filename);
 
-		// resize the vertex array to fit the level size
-		m_vertices.setPrimitiveType(sf::Lines);
-		m_vertices.resize(mColumns * mRows * 8);
-		
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Lines);
+        m_vertices.resize(mColumns * mRows * 8);
+        
         int ratioHeigth = screenHeigth * 0.98 / mRows;
         int ratioWidth = screenWidth * 0.98 / mColumns;
 
-		// populate the vertex array, with one quad per tile
-		for (int i = 0; i < mColumns; i++)
-		{
-			for (int j = 0; j < mRows; j++)
-			{
-				// get a pointer to the current tile's quad
-				sf::Vertex* quad = &m_vertices[(i + j * mColumns) * 8];
+        // populate the vertex array, with one quad per tile
+        for (int i = 0; i < mColumns; i++)
+        {
+            for (int j = 0; j < mRows; j++)
+            {
+                // get a pointer to the current tile's quad
+                sf::Vertex* quad = &m_vertices[(i + j * mColumns) * 8];
 
                 // left
                 quad[0].position = sf::Vector2f(i * ratioHeigth, (j + 1) * ratioHeigth);
                 quad[1].position = sf::Vector2f(i * ratioHeigth, j * ratioHeigth);
 
-				// upper
+                // upper
                 quad[2].position = sf::Vector2f(i * ratioWidth, j * ratioWidth);
                 quad[3].position = sf::Vector2f((i + 1) * ratioWidth, j * ratioWidth);
 
@@ -53,20 +53,20 @@ public:
                         quad[(z * 2) + 1].color = sf::Color::Black;
                     }
                 }
-			}
-		}
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
 private:
 
-	void load(string filename)
-	{
-		ifstream infile(filename);
+    void load(string filename)
+    {
+        ifstream infile(filename);
 
         // Read the number of Columns and Rows
-		infile >> mColumns >> mRows;
+        infile >> mColumns >> mRows;
 
         // Read the star and end cells positions
         infile >> mStartCellRow >> mStartCellColumn >> mEndCellRow >> mEndCellColumn;
@@ -82,21 +82,21 @@ private:
                 mCells.push_back(bitset<4>(tempBitset));
             }
         }
-	}
+    }
 
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-	{
-		// apply the transform
-		states.transform *= getTransform();
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
 
-		// draw the vertex array
-		target.draw(m_vertices, states);
-	}
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
 
-	sf::VertexArray m_vertices;
-	
+    sf::VertexArray m_vertices;
+    
     int mColumns, mRows, mStartCellRow, mStartCellColumn, mEndCellRow, mEndCellColumn;
 
     vector<bitset<4>> mCells;
-	
+    
 };
